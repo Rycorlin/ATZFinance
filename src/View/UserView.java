d152761ebbb3c1f74a8a6a15120b36d183630fee
Rycorlin <Rycorlin@gmail.com>
2020-09-30 04:48:32 -0400

    Added User Table, to create an array JavaFX can use, and created a UserView that uses that User table to create a table list with the data

---
+++ src/View/UserView.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import atzfinance.User;
+import atzfinance.UserTable;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author rycor
+ */
+public class UserView extends Application 
+{
+    public static void main(String[] args) 
+    {
+        Application.launch(args);
+    }
+ 
+    @SuppressWarnings("unchecked")
+    @Override
+    public void start(Stage stage) 
+    {
+        // Create a TableView with a list of persons
+        TableView<User> table = new TableView<>();
+        // Add rows to the TableView
+        table.getItems().addAll(UserTable.getPersonList());
+        // Add columns to the TableView
+        table.getColumns().addAll(UserTable.getFirstNameColumn(),
+            UserTable.getLastNameColumn(),UserTable.getUserNameColumn(),
+            UserTable.getSocialSecurityColumn());
+         
+        // Set the column resize policy to constrained resize policy
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        
+        // Set the Placeholder for an empty table
+        table.setPlaceholder(new Label("No visible columns and/or data exist."));
+         
+        // Create the VBox
+        VBox root = new VBox();
+        // Add the Table to the VBox
+        root.getChildren().add(table);
+        // Set the Padding and Border for the VBox
+        root.setStyle("-fx-padding: 10;" +
+                "-fx-border-style: solid inside;" +
+                "-fx-border-width: 2;" +
+                "-fx-border-insets: 5;" +
+                "-fx-border-radius: 5;" +
+                "-fx-border-color: blue;");
+         
+        // Create the Scene
+        Scene scene = new Scene(root);
+        // Add the Scene to the Stage
+        stage.setScene(scene);
+        // Set the Title of the Stage
+        stage.setTitle("ATZ Finance");
+        // Display the Stage
+        stage.show();       
+    }
+}
