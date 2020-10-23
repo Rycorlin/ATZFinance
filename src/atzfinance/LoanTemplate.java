266e91b250989ed003832d293163a5821e46119b
Kara <xxanimewishesxx@gmail.com>
2020-10-22 22:44:24 -0400

    updated the loan portion of the application to reflect a template design

---
+++ src/atzfinance/LoanTemplate.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atzfinance;
+
+/**
+ *
+ * @author xxani
+ */
+public abstract class LoanTemplate
+{
+    public final void buildLoan()
+    {
+    setInterestRate();
+    setTerm();
+    setAmount();
+
+    }
+    
+    public abstract double setInterestRate();
+    public abstract int setTerm();
+    public abstract double setAmount();
+    public abstract void setBalanceDue(double n);
+}
