a928d2c7482edb6e48331e8a728ed1bd2a5fea4c
Rycorlin <Rycorlin@gmail.com>
2020-10-24 00:53:35 -0400

    User List/Creation updates

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -6,6 +6,7 @@ import View.LoginView;
 import Model.Customer;
 import Model.User;
 import Model.UserTable;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
@@ -21,14 +22,17 @@ public class LoginController {
     private HomeView hv;
     private CreateAccountView newAccount;
     private UserTable uTable = new UserTable();
+    Stage stage;
+    ArrayList<User> al = new ArrayList();
 
     public LoginController(String[] args) {
 
         Application.launch(LoginView.class, args);
     }
 
-    public LoginController(LoginView loginView) {
+    public LoginController(LoginView loginView, Stage stage) {
         this.loginView = loginView;
+        this.stage = stage;
     }
 
     public boolean login(String username, String password, Stage primaryStage) {
@@ -48,6 +52,23 @@ public class LoginController {
     public boolean login(String username, String password) {
         return username.equalsIgnoreCase(user) && password.equals(pw);
     }
+    
+    
+    //Done in HomeView atm because cant get button to work from here - RM
+   /* public void logoff()
+    {
+       
+       
+       hv.logoutButton.setOnAction((ActionEvent event) ->
+       {
+           System.out.println("Hey");
+           loginView.start(stage);
+           
+           
+           
+       });
+        
+    }*/
 
     //From loginview open up new CreateAccountView UI when creating new account
     public void createAcount(Stage primaryStage) throws Exception {
@@ -56,18 +77,17 @@ public class LoginController {
 
         //Action on click "Create Account" for new account sends to LoginController
         newAccount.register.setOnAction((ActionEvent event) -> {
-            
+
             User newUser = new User(newAccount.getFirstName(), newAccount.getLastName(), newAccount.getUsername(), 10);
+
             uTable.addUser(newUser);
-            
+
             System.out.println(uTable.getUser(0).getFirstName());
             System.out.println(uTable.getSize());
-            
+
             //System.out.println(newUser.getFirstName());
             //System.out.println(newUser.getLastName());
             //System.out.println(newUser.getUserName());
-            
-            
             this.login(user, user, primaryStage); //Do we setup auto login after register or send them to LoginView to login
 
         });
